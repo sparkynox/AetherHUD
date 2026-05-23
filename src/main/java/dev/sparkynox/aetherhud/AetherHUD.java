e5e409365818982323fd90da7cef50a8923e08aa
ＳＰＡＲＫＹ'ＮＯＸ <senpaineji7@gmail.com>
2026-05-23 21:14:49 +0530

    Add files via upload

--- src/main/java/dev/sparkynox/aetherhud/AetherHUD.java
+++ src/main/java/dev/sparkynox/aetherhud/AetherHUD.java
@@ -1,1 +1,38 @@
+package dev.sparkynox.aetherhud;
 
+import dev.sparkynox.aetherhud.config.HudConfig;
+import dev.sparkynox.aetherhud.editor.HudEditorScreen;
+import dev.sparkynox.aetherhud.hud.HudRenderer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class AetherHUD implements ClientModInitializer {
+
+    public static final String MOD_ID = "aetherhud";
+
+    // H key opens the drag editor
+    public static KeyBinding editorKey;
+
+    @Override
+    public void onInitializeClient() {
+        HudRenderer.init();
+        HudConfig.load(); // load saved positions on top of defaults
+
+        editorKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.aetherhud.editor",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_H,
+            "AetherHUD"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (editorKey.wasPressed() && client.currentScreen == null) {
+                client.setScreen(new HudEditorScreen());
+            }
+        });
+    }
+}
